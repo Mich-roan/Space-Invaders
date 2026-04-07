10b395ae8ef48668f26b9cce5baeaacb1de1c3c3
Mich-Roan <michelle.rodriguez@uteca.edu.mx>
2026-04-06 20:34:30 -0600

    disparo

--- Assets/Scripts/AsteroidManager.cs
+++ Assets/Scripts/AsteroidManager.cs
@@ -11,9 +11,16 @@ public class AsteroidManager : MonoBehaviour
   private float spawnInterval = 2f;
   [SerializeField]
   private UnityEvent<Transform> onAsteroidDestroyed;
+  [SerializeField]
+  private int numberOfAsteroids = 10;
   private void Start()
     {
-        Invoke("SpawnAsteroid", spawnInterval);
+        float initialDelay = 0f;
+        for (int i = 0; i < numberOfAsteroids; i++)
+        {
+          Invoke("SpawnAsteroid", initialDelay);  
+          initialDelay += spawnInterval;
+        } 
     }
     private void SpawnAsteroid()
     {
@@ -24,8 +31,6 @@ public class AsteroidManager : MonoBehaviour
         Asteroid asteroid = asteroidPool.GetCurrentObject().GetComponent<Asteroid>();
         asteroid.SetTarget(target);
         asteroid.OnAsteroidDestroyed.AddListener(OnAsteroidDestroyed);
-        spawnInterval = Random.Range(1f, 3f);
-        Invoke("SpawnAsteroid", spawnInterval);
     }
   public void OnAsteroidDestroyed(Transform asteroid)
     {
